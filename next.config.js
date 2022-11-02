bd70d5a47517d327a85c361bc86d82a37df330c9
Carlos Vouking <carlosvouking@gmail.com>
2022-11-02 08:48:16 +0100

    updtout

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    images: {
-        unoptimized: true
-    },
     reactStrictMode: true,
-<<<<<<< HEAD
     images: {
         unoptimized: true,
     },
 }
-=======
-}
->>>>>>> 65b3529680b4e55a27c2a66d63310cd151df62f9
